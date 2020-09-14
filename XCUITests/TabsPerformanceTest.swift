2c2a32297381ec8a7356e1c6926ade3cc42c17e9
Richard Pappalardo <rpappalax@gmail.com>
2020-09-14 13:41:13 -0700

    feat: parameterize LaunchArguments to handle different archive files

--- XCUITests/TabsPerformanceTest.swift
+++ XCUITests/TabsPerformanceTest.swift
@@ -1,24 +1,22 @@
 import XCTest
 
-//let firstURL = "mozilla.org"
-//let secondURL = "mozilla.org/en-US/book"
-//let fullFirstURL = "https://www.mozilla.org/en-US/"
-
 class TabsPerformanceTest: BaseTestCase {
 let fixtures = ["testPerfTabs10": "testTabsPerformance-brower.db"]
 
-        let testWithDB = ["testPerfDbLoader","testPerfTabs10Setup"]
-//        let testTabsDB = "testTabsPerformance-browser.db"
-        let testTabsDB = "testHistoryDatabase4000-browser.db"
-
-        let testTabsStateArchive = "tabsState5.archive"
+    let testWithTabs:[String:String] = ["testPerfTabs5": "tabsState5.archive","testPerfTabs5Setup": "tabsState5.archive"]
 
         override func setUp() {
-            // Test name looks like: "[Class testFunc]", parse out the function name
+            // Test name looks like: "[Class testFunc]", parse out function name
             let parts = name.replacingOccurrences(of: "]", with: "").split(separator: " ")
-            let key = String(parts[1])
-            if testWithDB.contains(key) {
-                launchArguments = [LaunchArguments.PerformanceTest, LaunchArguments.SkipIntro, LaunchArguments.SkipWhatsNew, LaunchArguments.SkipETPCoverSheet,  LaunchArguments.LoadDatabasePrefix + testTabsDB, LaunchArguments.LoadTabsStateArchive + testTabsStateArchive]
+            let functionName = String(parts[1])
+            let archiveName = testWithTabs[functionName]
+            
+            launchArguments = [LaunchArguments.PerformanceTest, LaunchArguments.SkipIntro, LaunchArguments.SkipWhatsNew, LaunchArguments.SkipETPCoverSheet]
+            
+            if testWithTabs.keys.contains(functionName) {
+                print("if!!")
+                print(archiveName!)
+                launchArguments.append(LaunchArguments.LoadTabsStateArchive + archiveName!)
             }
             super.setUp()
         }
@@ -28,7 +26,15 @@ let fixtures = ["testPerfTabs10": "testTabsPerformance-brower.db"]
         }
 
 
-    func testPerfDbLoader() {
+    func testPerfTabs5() {
+        // load application and wait
+        // observe if test DBs are successfully loaded
+          do {
+              sleep(100)
+          }
+    }
+    
+    func testPerfTabs10() {
         // load application and wait
         // observe if test DBs are successfully loaded
           do {
@@ -36,8 +42,7 @@ let fixtures = ["testPerfTabs10": "testTabsPerformance-brower.db"]
           }
     }
 
-
-    func testPerfTabs10Setup() {
+    func testPerfTabs5Setup() {
         let fileName = "/Users/rpappalax/git/firefox-ios-NEW-ISSUE/test-fixtures/topdomains_10.txt"
         var contents = ""
         var urls = [String]()
@@ -50,23 +55,16 @@ let fixtures = ["testPerfTabs10": "testTabsPerformance-brower.db"]
             print("COULDNT LOAD")
         }
 
-
         //navigator.performAction(Action.TogglePrivateMode)
         navigator.goto(NewTabScreen)
 //        let urls = ["yahoo.com", "google.com", "cnn.com"]
 //
-  
         for url in urls {
             print(url)
             navigator.openNewURL(urlString: url)
             waitUntilPageLoad()
         }
-
-
         waitForTabsButton()
         navigator.goto(TabTray)
     }
-
-
-
 }
